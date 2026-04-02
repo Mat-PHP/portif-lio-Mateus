3dba28a1ff916458ed13473d1d0ad5660b24f873
MATEUS FERREIRA SALUSTIANO <mateus.salustiano@senaisp.edu.br>
2026-04-02 09:02:37 -0300

    Built web
    Branch: main
    Commit: bac350301663db3307af41a31aec612858e58391
    
    package:peanut 6.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "562b0badeb5892f8fdcd0f521370f759
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "6283f6abca1f92627eebd8d56555e7ba",
+"flutter_bootstrap.js": "ad3e5f89aea84c7bf14f0b1c0da07248",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
